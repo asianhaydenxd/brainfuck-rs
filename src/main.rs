589f561e77ea6040f5889a6abba909e19359c8dd
Hayden <asianhaydenxd@gmail.com>
2022-11-20 14:10:29 -0500

    Add test and extend parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-enum Tokens {
+#[derive(Eq, PartialEq, Debug)]
+enum Token {
     Left,
     Right,
     Plus,
@@ -9,14 +10,50 @@ enum Tokens {
     Comma,
 }
 
-fn parse(code: String) {
+fn parse(code: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
 
+    for character in code.chars() {
+        match character {
+            '<' => tokens.push(Token::Left),
+            '>' => tokens.push(Token::Right),
+            '+' => tokens.push(Token::Plus),
+            '-' => tokens.push(Token::Minus),
+            '[' => tokens.push(Token::LeftBracket),
+            ']' => tokens.push(Token::RightBracket),
+            '.' => tokens.push(Token::Dot),
+            ',' => tokens.push(Token::Comma),
+            _ => {},
+        };
+    }
+
+    tokens
 }
 
 fn interpret(code: String) {
+    println!("{:?}", parse(code));
     println!("Interpreter complete");
 }
 
 fn main() {
-    println!("Hello world!");
+    interpret("++++[->+++++<]>.".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(parse("<>+-[].,".to_string()), vec![
+            Token::Left,
+            Token::Right,
+            Token::Plus,
+            Token::Minus,
+            Token::LeftBracket,
+            Token::RightBracket,
+            Token::Dot,
+            Token::Comma,
+        ]);
+    }
 }
