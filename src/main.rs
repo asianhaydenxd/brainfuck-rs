e0f56c64420eb3ff3fb33a8a7c1ac8b6a1b20e1e
Hayden <asianhaydenxd@gmail.com>
2022-11-22 10:25:47 -0500

    Implement partial interpreter

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ enum Token {
     Comma,
 }
 
-fn parse(code: String) -> Vec<Token> {
+fn lex(code: String) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
 
     for character in code.chars() {
@@ -30,9 +30,45 @@ fn parse(code: String) -> Vec<Token> {
     tokens
 }
 
-fn interpret(code: String) {
-    println!("{:?}", parse(code));
-    println!("Interpreter complete");
+enum Node {
+    Shift(i32),
+    Add(i32),
+    Loop(Vec<Node>),
+    Print,
+    Input,
+}
+
+struct Interpreter {
+    tape: Vec<u8>,
+    pointer: usize,
+}
+
+impl Interpreter {
+    fn new() {
+        Interpreter {
+            tape: Vec::new(),
+            pointer: 0
+        }
+    }
+
+    fn subinterpret(&self, nodes: Vec<Node>) {
+        for node in nodes.iter() {
+            match node {
+                Shift(n) => pointer += n,
+                Add(n) => tape[pointer] += n,
+                Loop(subnodes) => while tape[pointer] > 0 {
+                    self.subinterpret(subnodes);
+                }
+                Print => {},
+                Input => {}
+            }
+        }
+    }
+}
+
+fn interpret(nodes: Vec<Node>) {
+    let mut interpreter: Interpreter = Interpreter::new();
+    interpreter.subinterpret(nodes);
 }
 
 fn main() {
@@ -45,7 +81,7 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(parse("<>+-[].,".to_string()), vec![
+        assert_eq!(lex("<>+-[].,".to_string()), vec![
             Token::Left,
             Token::Right,
             Token::Plus,
